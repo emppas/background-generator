d6ab03966e4d4bdff551599905ffaf3c2e7ec144
emppas <contactokonko@gmail.comm>
2018-08-22 18:56:41 +0100

    adding starting project

---
+++ script.js
@@ -0,0 +1,61 @@
+const array = [1, 2, 10, 16];
+// const double = []
+// const newArray = array.forEach((num) => {
+// 	double.push(num * 2);
+// })
+// console.log('forEach', double);
+
+const mapArray = array.map((num => num * 2);
+console.log('map', mapArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var css = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("gradient");
+
+// function setGradient(){
+// 	body.style.background = "linear-gradient(to right,"
+// 	+ color1.value + "," + color2.value + ")";
+
+// 	css.textContent = body.style.background + ";";
+// }
+
+
+// // console.log(css);
+// // console.log(color1);
+// // console.log(color2);
+
+
+// color1.addEventListener("input", setGradient);
+
+// color2.addEventListener("input", setGradient);
+
+
+
+	
